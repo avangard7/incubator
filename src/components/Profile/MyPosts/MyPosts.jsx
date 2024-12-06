dc82287790da0d648a85afe4e0ee5cd044c600c4
avangard7 <razor6309@gmail.com>
2024-12-06 15:36:34 +0300

    Lesson 31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,28 @@
+import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
   // debugger 
 
+  let newPostElement = React.createRef();  
+// ------------------------------------------------------
+ const addPost =  () => { 
+  // debugger   
+  let text = newPostElement.current.value;     
+  console.log( text );   
+  }; 
+// ------------------------------------------------------
+
   return (
     <div className={s.postsBlock}>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={ newPostElement }></textarea>  
         </div>
         <div>
 
-          <button className={s.postsButton}>
+          <button className={s.postsButton} onClick={ addPost } >
             Add post
           </button>
 
